52d2f8e76a85f9ed4dd991a66e215df1677dbbad
Leon Jiang <leon@getsnaptravel.com>
2018-04-01 20:58:44 -0400

    dual screen dynamic bg

--- dynamicbg.sh
+++ dynamicbg.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 killall xwinwrap
-xwinwrap -b -fs -sp -fs -nf -ov -- mplayer -loop 0 -wid WID ~/Videos/waterfall.gif &
+xwinwrap -ov -g 2560x1440+1080+376 -- mplayer -loop 0 -wid WID ~/Videos/light.mov &
+xwinwrap -ov -g 1080x1920 -- mplayer -wid WID -vf rotate=1 -loop 0 ~/Videos/glow.mp4 &
